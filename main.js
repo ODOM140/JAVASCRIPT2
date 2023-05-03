c6855137caf7eb75c7852a01be8f570fe2b7190c
ODOM140 <odomenche123@gmail.com>
2023-05-03 12:57:04 +0100

    My First Commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// dom selectors
+// document.querySelector('body').innerHTML = '<h1>hello world</h1>'
+
+// variables
+var myName = 'chinecherem'
+var myAge = 4
+var adult = false
+let color = 'green'
+var number1 = 10
+var number2 = 5
+var btn = document.querySelector('button')
+color = 'red'
+// var btn = document.getElementsByTagName('button')
+var yob = 2010
+
+
+// string methods
+// console.log(myName.length)
+// console.log(myName.toUpperCase())
+
+
+
+// testing types of variables
+// console.log(typeof myName)
+// console.log(typeof myAge)
+// console.log(typeof adult)
+// console.log(typeof color)
+
+// making use of variables
+// console.log('my name is  '+myName+' and i am '+myAge+' years old, and my favourite colour is '+ color) 
+// console.log(number1+number2)
+
+// btn.onclick = function(){
+//     console.log(yob-=1)
+//     var age = 2023-yob
+//     console.log(age)
+// if(age < 18){
+//     console.log('sorry, you can not party with the big boys')
+// }else if(age > 40){
+//     console.log('too old to attend this party')
+// }else{
+//     console.log('welcome to the party fam')
+// }
+
+// }
+
+var balance =0
+
+if (balance >= 1) {
+    console.log('you can buy');
+} else {
+    console.log('sorry you can not buy');
+    
+}
+
